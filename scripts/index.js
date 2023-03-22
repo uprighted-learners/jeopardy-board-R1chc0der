79a7f4972fac7c6a3d2d9458b6d24fd4e63182a3
Rich <c0deyes@protonmail.com>
2023-03-22 00:33:21 -0400

    jeopardy on it's way

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,39 @@
-// Do not change the import statement
-import placeholderQuestions from "./placeholder-questions.js";
-console.log({ placeholderQuestions });
+// click button - question from json object
+// question display via <p> tag via click
+// input box responses to question from <p>
+// guess button compares response from <p>/json object
+
+ 
+
+let questionButtons = document.querySelectorAll("td button") 
+    for (let questionButton of questionButtons ){
+        questionButton.addEventListener("click", () =>{
+            log("button clicked")
+
+        })
+    }
+
+// 
+let log = console.log
+//log("testing the log")
+
+function placeholderQuestions(){
+    const data = require("./questions.json") // passing our file location
+    //.then(res => res.json())
+    //.then(data => {
+        let members = data.placeholderQuestions; 
+        //log(members);
+
+        // Used a for/of loop to cycle through the array
+        for(let member of members){
+          //  log(m); // logs each member object
+            log(member.category);
+        }
+
+/*     })
+    .catch(error => {
+        log(error);
+    } ) */
+
+}
+placeholderQuestions();
