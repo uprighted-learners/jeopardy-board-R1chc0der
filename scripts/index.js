f3338daf839452a55f249ef681ba3996b0f8c56e
Rich <c0deyes@protonmail.com>
2023-03-22 21:24:41 -0400

    Jv2

--- scripts/index.js
+++ scripts/index.js
@@ -4,11 +4,12 @@
 // guess button compares response from <p>/json object
 
  
-
+// get question in console.log then to get i a p tag
 let questionButtons = document.querySelectorAll("td button") 
     for (let questionButton of questionButtons ){
         questionButton.addEventListener("click", () =>{
             log("button clicked")
+            //alert("button works")
 
         })
     }
@@ -37,3 +38,41 @@ function placeholderQuestions(){
 
 }
 placeholderQuestions();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+function placeholderQuestions(){
+    const data = require("./questions.json") // passing our file location
+    //.then(res => res.json())
+    //.then(data => {
+        let members = data.placeholderQuestions; 
+        //log(members);
+
+        // Used a for/of loop to cycle through the array
+        for(let member of members){
+          //  log(m); // logs each member object
+            log(member.category);
+        }
+
+/*     })
+    .catch(error => {
+        log(error);
+    } ) */
+
+//}
+//placeholderQuestions(); 
